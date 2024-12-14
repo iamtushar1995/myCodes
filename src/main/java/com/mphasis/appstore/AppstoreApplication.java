67f8d2abc8c74de358b90630385a293fdd21e4fd
iamtushar1995 <165132052+iamtushar1995@users.noreply.github.com>
2024-12-14 21:28:28 +0530

    Update AppstoreApplication.java

--- src/main/java/com/mphasis/appstore/AppstoreApplication.java
+++ src/main/java/com/mphasis/appstore/AppstoreApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AppstoreApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Appstore Consumer Started");
 		SpringApplication.run(AppstoreApplication.class, args);
 	}
 
